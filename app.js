0fa719606bccd016c7776a21d6dac1598240d6bf
owaisj <jamil.owais@gmail.com>
2019-05-14 16:54:17 -0500

    Constructor and ajax call for geocode

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-
+    //Map
     var mymap = L.map('map').setView([38.9072, -77.0369], 13);
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
@@ -13,4 +13,37 @@ $(document).ready(function(){
     .bindPopup('Washington DC')
     .openPopup();
 
+    //Cities
+    function City(name) {
+        this.name = name,
+        this.geocode = function() {
+            $.ajax({
+                url: queryURL,
+                method: 'GET'
+            }).then(function(response){
+                
+            }).catch()
+        },
+        this.display = function() {
+            console.log(`Name: ${this.name}\nCoordinate: ${this.geocode}`)
+        }
+
+    }
+
+    async function getGeocode(city) {
+        let queryURL = `https://nominatim.openstreetmap.org/?format=json&city=${city}`;
+        $.ajax({
+            url: queryURL,
+            method: 'GET'
+        }).then(await function(response){
+            let coordinates = {
+                lat: parseFloat(response[0].lat),
+                lon: parseFloat(response[0].lon)
+            };
+            return coordinates;
+        }).catch()
+    }
+
+    //let DC = new City('Washington DC');
+    console.log(getGeocode('Washington DC'));
 });
